b0ed9e9305844245084a3c1535f82bd873dda199
CondensedMilk7 <82287873+CondensedMilk7@users.noreply.github.com>
2021-08-11 19:52:19 +0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListingComponent } from './listing/listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListingComponent
   ],
   imports: [
     BrowserModule
